c46443594fa9945bbca948d31461398d01cc001a
licoris <klepehin.k.a@gmail.com>
2026-02-12 18:10:41 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru. netology.service;
+
+
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void MoreThen1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void lessThen1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int expected = 200;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void EqualTo1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+}
